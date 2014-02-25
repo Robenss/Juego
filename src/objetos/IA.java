508a96a09d18464ff21079e98cd9dd8b4c2a4740
Rubén <robensbarrabajauno@gmail.com>
2014-02-25 23:56:34 +0100

    Clases
    algunas clases

--- src/objetos/IA.java
+++ src/objetos/IA.java
@@ -1,63 +1,64 @@
 package objetos;
 
-public class Player {
-
+public class IA {
+	
 	private int x;
 	private int y;
 	private int hp;
 	private int mp;
 	private int atk;
 	private int def;
-
-	public Player(){
+	
+	public IA(){
 		super();
 	}
-	
+
 	public int getX() {
 		return x;
 	}
-	
+
 	public void setX(int x) {
 		this.x = x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
-	
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 	public int getHp() {
 		return hp;
 	}
-	
+
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
-	
+
 	public int getMp() {
 		return mp;
 	}
-	
+
 	public void setMp(int mp) {
 		this.mp = mp;
 	}
-	
+
 	public int getAtk() {
 		return atk;
 	}
-	
+
 	public void setAtk(int atk) {
 		this.atk = atk;
 	}
-	
+
 	public int getDef() {
 		return def;
 	}
-	
+
 	public void setDef(int def) {
 		this.def = def;
 	}
+
 }
---
+++ src/objetos/Mapa.java
@@ -0,0 +1,12 @@
+package objetos;
+
+import java.util.LinkedList;
+
+public class Mapa {
+
+	private LinkedList<Player> players;
+	
+	public Mapa(){
+		super();
+	}
+}
