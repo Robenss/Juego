a883511f1000836175d08036a78275cb584d8d6f
Rubén <robensbarrabajauno@gmail.com>
2014-02-25 23:42:44 +0100

    Push
    p

---
+++ src/objetos/Player.java
@@ -0,0 +1,29 @@
+package objetos;
+
+public class Player {
+
+	private int x;
+	private int y;
+
+	public Player(){
+		super();
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
